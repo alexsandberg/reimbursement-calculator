ee8ed4745a9efcf96ac70a896e7bc57f7d440e68
Alex <alexsandberg7@gmail.com>
2024-10-14 15:51:13 -0400

    exit if csv read error

--- src/reimbursement/csv.py
+++ src/reimbursement/csv.py
@@ -1,13 +1,16 @@
 import pandas as pd
+from pandas import DataFrame
 
-from src.reimbursement.project import Project, ProjectSet
+import sys
 
-def read_csv(file_path):
+def read_csv(file_path: str) -> DataFrame:
     data = None
     try:
         data = pd.read_csv(file_path)
     except FileNotFoundError:
         print(f"Error: File '{file_path}' not found.")
+        sys.exit(1)
     except Exception as e:
         print(f"An error occurred: {e}")
+        sys.exit(1)
     return data
