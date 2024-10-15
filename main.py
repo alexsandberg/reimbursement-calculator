1bdd9376e0bcad6acb4ec4eeb67af6e2edc7dbdb
Alex <alexsandberg7@gmail.com>
2024-10-15 12:10:09 -0400

    main docstring

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from src.reimbursement.reimbursement import calculate_reimbursement
 
 
 def main():
+    """
+    Main driver function for reimbursement calculator program.
+    Data is loaded from a csv file and initiated as a new ProjectSet instance.
+    Reimbursement total is then calculated from the ProjectSet.
+    """
     if len(sys.argv) != 2:
         print(
             "Incorrect arguments supplied. Intended usage: python main.py <csv_file_path>"
